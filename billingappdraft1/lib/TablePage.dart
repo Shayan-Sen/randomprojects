0a3bc03f56ea4d98a9ef3aae7f01e7519a72d3a5
Shayan Sen <senshayan2103@gmail.com>
2024-02-21 16:33:08 +0530

    bappdrft1 | 21/2/2024 4:31 PM |

--- billingappdraft1/lib/TablePage.dart
+++ billingappdraft1/lib/TablePage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class Dtblewdjt extends StatelessWidget {
@@ -13,7 +15,27 @@ class Dtblewdjt extends StatelessWidget {
         elevation: 10,
         title: Text("Table Page"),
       ),
-      // body: DataTable(columns:, rows: List.empty()),
+      body: DataTable(
+        border: TableBorder.all(),
+        columns: [
+          DataColumn(label: Text("Column1")),
+          DataColumn(label: Text("Colun2"))
+        ],
+        rows: [
+          DataRow(cells: [
+            DataCell(SizedBox(height: 10, width: 30)),
+            DataCell(SizedBox(height: 10, width: 30)),
+          ]),
+          DataRow(cells: [
+            DataCell(SizedBox(height: 10, width: 30)),
+            DataCell(SizedBox(height: 10, width: 30)),
+          ]),
+          DataRow(cells: [
+            DataCell(SizedBox(height: 10, width: 30)),
+            DataCell(SizedBox(height: 10, width: 30)),
+          ]),
+        ],
+      ),
     );
   }
 }
